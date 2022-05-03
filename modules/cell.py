0181ae3c1499f7bf03afb75f0f2d1dc935f126b9
Zakhar Kalachev <zackfnx@yandex.ru>
2022-05-03 18:13:09 +0300

    Update cell.py

--- modules/cell.py
+++ modules/cell.py
@@ -1,18 +1,50 @@
 import pygame
+from enum import IntEnum
+from settings import *
+
+
+class CellType(IntEnum):
+    EMPTY = 0
+    BLOCKED = 1
+    START = 2
+    END = 3
+    PATH = 4
 
 
 class Cell:
-    def __init__(self, position, size, outline, fill_color, outline_color):
-        self.outline_surface = pygame.surface.Surface((size + 2*outline, size + 2*outline))
-        self.fill_surface = pygame.surface.Surface((size, size))
-
-        self.outline_surface.fill(outline_color)
-        self.fill_surface.fill(fill_color)
+    def __init__(self, position):
+        self.outline_surface = pygame.surface.Surface((CELL_SIZE + 2 * CELL_OUTLINE, CELL_SIZE + 2 * CELL_OUTLINE))
+        self.fill_surface = pygame.surface.Surface((CELL_SIZE, CELL_SIZE))
 
         self.outline_rect = pygame.rect.Rect(position, self.outline_surface.get_size())
-        self.fill_rect = pygame.rect.Rect((position[0] + outline, position[1] + outline), self.fill_surface.get_size())
+        self.fill_rect = pygame.rect.Rect(
+            (position[0] + CELL_OUTLINE, position[1] + CELL_OUTLINE),
+            self.fill_surface.get_size()
+        )
 
-    def update(self, mouse_position): pass
+        self.cell_type = CellType.EMPTY
+        self.set_outline_color(OUTLINE_COLOR)
+
+    def set_outline_color(self, color):
+        self.outline_surface.fill(color)
+
+    def set_fill_color(self, color):
+        self.fill_surface.fill(color)
+
+    def clicked(self, mouse_position):
+        return self.outline_rect.collidepoint(mouse_position)
+
+    def update(self):
+        if self.cell_type == CellType.EMPTY:
+            self.set_fill_color(EMPTY_CELL_COLOR)
+        if self.cell_type == CellType.BLOCKED:
+            self.set_fill_color(BLOCKED_CELL_COLOR)
+        if self.cell_type == CellType.START:
+            self.set_fill_color(START_CELL_COLOR)
+        if self.cell_type == CellType.END:
+            self.set_fill_color(END_CELL_COLOR)
+        if self.cell_type == CellType.PATH:
+            self.set_fill_color(PATH_CELL_COLOR)
 
     def draw(self, surface):
         surface.blit(self.outline_surface, self.outline_rect.topleft)
