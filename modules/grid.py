11ce0c9a878148341655a094d178fe113ba9af37
Zakhar Kalachev <zackfnx@yandex.ru>
2022-05-03 18:13:12 +0300

    Update grid.py

--- modules/grid.py
+++ modules/grid.py
@@ -1,32 +1,33 @@
 import pygame
-from modules.cell import Cell
+from modules.cell import Cell, CellType
+from settings import *
 
 
 class Grid:
-    def __init__(self, settings, position):
-        self.settings = settings
-        self.position = position
-
+    def __init__(self):
         self.cells = []
-        for i in range(self.settings['grid']['rows']):
-            for j in range(self.settings['grid']['columns']):
-                cell_size = self.settings['grid']['cell_size']
-                cell_outline = self.settings['grid']['outline']
-
-                cell_offset = cell_size + cell_outline
-                cell_position = (j * cell_offset, i * cell_offset)
-
-                cell_fill_color = self.settings['color']['empty_cell']
-                cell_outline_color = self.settings['color']['outline']
-
-                self.cells.append(Cell(cell_position, cell_size, cell_outline, cell_fill_color, cell_outline_color))
+        for i in range(GRID_ROWS):
+            for j in range(GRID_COLUMNS):
+                self.cells.append(Cell((j * (CELL_SIZE + CELL_OUTLINE), i * (CELL_SIZE + CELL_OUTLINE))))
 
         self.grid_surface = pygame.surface.Surface(self.cells[-1].outline_rect.bottomright)
 
-    def update(self, mouse_position): pass
+    def get_clicked_cell(self, mouse_position):
+        for index, cell in enumerate(self.cells):
+            if cell.clicked(mouse_position):
+                return index
+        return None
+
+    def reset(self):
+        for cell in self.cells:
+            cell.cell_type = CellType.EMPTY
+
+    def update(self):
+        for cell in self.cells:
+            cell.update()
 
     def draw(self, surface):
         for cell in self.cells:
             cell.draw(self.grid_surface)
 
-        surface.blit(self.grid_surface, self.grid_surface.get_rect(center=self.position))
+        surface.blit(self.grid_surface, self.grid_surface.get_rect())
